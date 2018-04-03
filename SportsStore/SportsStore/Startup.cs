c8041e2e0f041625034ac767311ef36bbf692440
lilithakobyan <lilithakobyan1994@mail.ru>
2018-04-03 18:29:54 +0400

    add cart

--- SportsStore/SportsStore/Startup.cs
+++ SportsStore/SportsStore/Startup.cs
@@ -28,6 +28,8 @@ namespace SportsStore
                     Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -39,7 +41,7 @@ namespace SportsStore
 
             app.UseStatusCodePages();
             app.UseStaticFiles();
-           
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
