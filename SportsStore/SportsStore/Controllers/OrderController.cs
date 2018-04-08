2fdb08231a7b7054e9a0b3e141f23d3a719d59ef
lilithakobyan <lilithakobyan1994@mail.ru>
2018-04-08 23:03:15 +0400

    orders

---
+++ SportsStore/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
